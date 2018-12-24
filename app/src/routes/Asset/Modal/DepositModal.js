6ce2c3b75b097e8c6f75d0aef536fadb3e93069a
weixiaoyi <2278095567@qq.com>
2018-12-24 17:58:26 +0800

    跨链充值

--- app/src/routes/Asset/Modal/DepositModal.js
+++ app/src/routes/Asset/Modal/DepositModal.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-
-import { Modal } from '../../../components/index';
+import { Modal, Icon } from '../../../components';
+import * as styles from './DepositModal.less';
 
 class DepositModal extends Component {
   render() {
-    return <Modal title="跨链充值">ahhahah</Modal>;
+    return (
+      <Modal title="跨链充值">
+        <div className={styles.bindAddress}>
+          <div>
+            已绑定地址：
+            <button>绑定新地址</button>
+          </div>
+          <ul>
+            <li>
+              <span>Bitcoin:</span>
+              <div>19zdMbaZnD8ze6XUZuVTYtVQ4URTYDFWbRw</div>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.publicAddress}>
+          <div>
+            <div className={styles.title}>公共多签托管地址</div>
+            <div className={styles.address}>
+              <span>Bitcoin:</span>3E3ZjvzDuMebxjZyYNyzkM9zZrDNeEVA29u6b
+            </div>
+            <div className={styles.warn}>
+              <Icon name="icon-jinggao" className={styles.icon} />
+              <span>请使用已绑定地址之一向公共多签托管地址转账，使用其他未绑定地址无法到账</span>
+            </div>
+          </div>
+          <div className={styles.back} />
+        </div>
+      </Modal>
+    );
   }
 }
 
