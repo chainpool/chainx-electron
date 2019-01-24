e48b921a0e504c7247af7b70ce6959923fceee5c
weixiaoyi <2278095567@qq.com>
2019-01-24 11:26:48 +0800

    盘口最新价颜色

--- app/src/routes/Trade/Handicap.js
+++ app/src/routes/Trade/Handicap.js
@@ -3,7 +3,7 @@ import SwitchPair from './Mixin/SwitchPair';
 
 import * as styles from './Handicap.less';
 import { Table } from '../../components';
-import { observer } from '../../utils';
+import { classNames, observer } from '../../utils';
 
 @observer
 class Handicap extends SwitchPair {
@@ -49,11 +49,19 @@ class Handicap extends SwitchPair {
       .fill()
       .concat(sellList.slice(Math.max(sellList.length - 5, 0), sellList.length));
     const dataSourceBuy = buyList.concat(new Array(5 - buyList.length > 0 ? 5 - buyList.length : 0).fill());
+
+    const isInSell = sellList.find((one = {}) => one.priceShow === currentPair.lastPriceShow);
+    const isInBuy = buyList.find((one = {}) => one.priceShow === currentPair.lastPriceShow);
+    console.log(!!isInSell, !!isInBuy);
     return (
       <div className={styles.handicap}>
         <div className={styles.title}>挂单列表</div>
         <Table {...setTableProps('red')} dataSource={dataSourceSell} />
-        <div className={styles.latestprice}>{currentPair.lastPriceShow}</div>
+        <div className={styles.latestprice}>
+          <span className={classNames(isInSell ? styles.red : null, isInBuy ? styles.green : null)}>
+            {currentPair.lastPriceShow}
+          </span>
+        </div>
         <Table {...setTableProps('green')} dataSource={dataSourceBuy} showHead={false} />
       </div>
     );
