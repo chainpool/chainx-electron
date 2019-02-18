8aa9fb2e0d923c63fc51064a66d6c64218f0ebaa
Yongfeng LI <wliyongfeng@gmail.com>
2019-02-18 16:58:41 +0800

    引入fetch polyfill

--- app/src/index.js
+++ app/src/index.js
@@ -6,6 +6,7 @@ import { configure } from 'mobx';
 import App from '@routes/App';
 import { default as store } from './store';
 import './index.less';
+import 'whatwg-fetch';
 
 // 不允许在动作外部修改状态
 configure({ enforceActions: 'always' });
