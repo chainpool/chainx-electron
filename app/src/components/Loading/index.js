1a7efddbf507f63ece6d7ff57760f87ac1a8fe20
weixiaoyi <2278095567@qq.com>
2019-03-13 16:01:39 +0800

    loadingIcon

--- app/src/components/Loading/index.js
+++ app/src/components/Loading/index.js
@@ -1,11 +1,18 @@
 import React, { PureComponent } from 'react';
+import loading from '../../resource/loading.gif';
 import * as styles from './index.less';
 
-class Loading extends PureComponent {
+class LoadingIcon extends PureComponent {
   render() {
     const { size = 20 } = this.props;
     return <i className={`iconfont icon-loading ${styles.loadingIcon}`} style={{ fontSize: size }} />;
   }
 }
 
-export default Loading;
+class Loading extends PureComponent {
+  render() {
+    return <img src={loading} />;
+  }
+}
+
+export { Loading };
