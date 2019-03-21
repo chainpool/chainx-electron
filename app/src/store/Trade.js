ed645916fe5f4e9825eb2ec1f71972b31451e8d7
weixiaoyi <2278095567@qq.com>
2019-03-21 18:55:03 +0800

    挂单成功

--- app/src/store/Trade.js
+++ app/src/store/Trade.js
@@ -61,7 +61,8 @@ export default class Trade extends ModelExtend {
       this.getOrderPairs();
       this.getQuotations();
       this.getAccountAssets();
-      this.getAccountOrder();
+      this.getCurrentAccountOrder();
+      this.getHistoryAccountOrder();
       this.getLatestOrder();
     }, 300);
   };
