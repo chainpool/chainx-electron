a298a70c5e9cd7a9a87abcbbd7b4cc1d6b736a01
weixiaoyi <2278095567@qq.com>
2019-06-03 10:59:59 +0800

    宽度调整

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron');
-const { autoUpdater } = require('electron-updater');
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const path = require("path");
 
 class AppUpdater {
   constructor() {
@@ -10,59 +10,61 @@ class AppUpdater {
 
 let mainWindow = null;
 
-if (process.env.NODE_ENV === 'development') {
-  require('electron-debug')();
+if (process.env.NODE_ENV === "development") {
+  require("electron-debug")();
 }
 
 /**
  * Add event listeners...
  */
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
   // app.quit();
 
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('before-quit', () => {
-  if (process.platform === 'darwin') {
+app.on("before-quit", () => {
+  if (process.platform === "darwin") {
     app.quitting = true;
   }
 });
 
-app.on('activate', () => {
-  if (process.platform === 'darwin' && mainWindow !== null) {
+app.on("activate", () => {
+  if (process.platform === "darwin" && mainWindow !== null) {
     mainWindow && mainWindow.show();
   }
 });
 
-app.on('ready', async () => {
+app.on("ready", async () => {
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1280,
+    width: 1320,
     minWidth: 800,
     minHeight: 600,
     height: 765,
-    webPreferences: { preload: path.join(__dirname, 'preload.js') },
+    webPreferences: { preload: path.join(__dirname, "preload.js") }
   });
 
   mainWindow.loadURL(
-    process.env.NODE_ENV === 'development' ? `http://localhost:8000` : `file://${__dirname}/app/build/index.html`
+    process.env.NODE_ENV === "development"
+      ? `http://localhost:8000`
+      : `file://${__dirname}/app/build/index.html`
   );
 
-  mainWindow.webContents.on('did-finish-load', () => {
+  mainWindow.webContents.on("did-finish-load", () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
     }
     mainWindow.show();
   });
 
-  mainWindow.on('close', event => {
-    if (process.platform === 'darwin') {
+  mainWindow.on("close", event => {
+    if (process.platform === "darwin") {
       if (app.quitting) {
         mainWindow = null;
       } else if (mainWindow !== null) {
@@ -72,7 +74,7 @@ app.on('ready', async () => {
     }
   });
 
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
 
