a39c5100bfba5a017a9535be2b1267b213349cdc
yznt <nsso@foxmail.com>
2019-06-06 14:40:56 +0800

    update

--- preload.js
+++ preload.js
@@ -1,7 +1,10 @@
 const electronStore = require('electron-store');
 const { shell, remote } = require('electron');
 
-window.electronStore = electronStore;
+window.electronStore = function(...args) {
+  return new electronStore(...args);
+};
+
 window.openExternal = url => shell.openExternal(url);
 
 window.fetchFromWs = ({ url, method, params = [], timeOut = 5000 }) => {
