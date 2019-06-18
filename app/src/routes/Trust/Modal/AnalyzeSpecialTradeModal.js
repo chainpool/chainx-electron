5facc51072b2c63d3715ea27078fea4e6a64db04
weixiaoyi <2278095567@qq.com>
2019-06-18 11:10:19 +0800

    特殊交易

--- app/src/routes/Trust/Modal/AnalyzeSpecialTradeModal.js
+++ app/src/routes/Trust/Modal/AnalyzeSpecialTradeModal.js
@@ -3,7 +3,11 @@ import { Button, Input, FormattedMessage, Modal } from '../../../components';
 import * as styles from './AnalyzeSpecialTradeModal.less';
 
 class AnalyzeSpecialTradeModal extends Component {
+  state = {
+    tx: '',
+  };
   render() {
+    const { tx } = this.state;
     const {
       model: { dispatch, openModal, closeModal },
     } = this.props;
@@ -17,7 +21,17 @@ class AnalyzeSpecialTradeModal extends Component {
           </Button>
         }>
         <div className={styles.AnalyzeSpecialTradeModal}>
-          <Input.Text label="待签原文" isTextArea rows={10} disabled />
+          <Input.Text
+            label="待签原文"
+            isTextArea
+            rows={10}
+            value={tx}
+            onChange={value =>
+              this.setState({
+                tx: value.trim(),
+              })
+            }
+          />
         </div>
       </Modal>
     );
--- app/src/routes/Trust/Modal/ConstructSpecialTradeModal.js
+++ app/src/routes/Trust/Modal/ConstructSpecialTradeModal.js
@@ -8,21 +8,17 @@ class ConstructSpecialTradeModal extends Component {
     sender: '',
     receiver: '',
     balance: '',
+    feeRate: '',
+    tx: '',
   };
   render() {
-    const { sender, receiver, balance } = this.state;
+    const { sender, receiver, balance, feeRate, tx } = this.state;
     const {
       model: { dispatch, openModal, closeModal },
     } = this.props;
 
     return (
-      <Modal
-        title={'构造特殊交易'}
-        button={
-          <Button size="full" type="confirm" onClick={() => {}}>
-            <FormattedMessage id={'Confirm'} />
-          </Button>
-        }>
+      <Modal title={'构造特殊交易'} button={null}>
         <div className={styles.ConstructSpecialTradeModal}>
           <Input.Address
             label={'发送方地址'}
@@ -47,6 +43,7 @@ class ConstructSpecialTradeModal extends Component {
           <InputHorizotalList
             left={
               <Input.Text
+                isDecimal="decimal"
                 label="金额"
                 value={balance}
                 suffix="BTC"
@@ -57,9 +54,21 @@ class ConstructSpecialTradeModal extends Component {
                 }}
               />
             }
-            right={<Input.Text label="手续费率" value={'kkkk'} suffix="Satoshis/KB" />}
+            right={
+              <Input.Text
+                isDecimal="decimal"
+                label="手续费率"
+                value={feeRate}
+                suffix="Satoshis/KB"
+                onChange={value => {
+                  this.setState({
+                    feeRate: value,
+                  });
+                }}
+              />
+            }
           />
-          <Input.Text label="待签原文" isTextArea rows={10} disabled />
+          <Input.Text label="待签原文" isTextArea rows={10} disabled value={tx} />
         </div>
       </Modal>
     );
