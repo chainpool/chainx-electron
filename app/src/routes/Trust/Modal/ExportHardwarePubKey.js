8791a4b88839bbaf57e558802bfdb2de798ac416
weixiaoyi <2278095567@qq.com>
2019-06-18 11:22:16 +0800

    特殊交易

--- app/src/routes/Trust/Modal/ExportHardwarePubKey.js
+++ app/src/routes/Trust/Modal/ExportHardwarePubKey.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, FormattedMessage, Icon, Modal } from '../../../components';
 import * as styles from './ExportHardwarePubKey.less';
+import { classNames } from '../../../utils';
 
 class ExportHardwarePubKey extends Component {
   state = {
@@ -20,39 +21,46 @@ class ExportHardwarePubKey extends Component {
             size="full"
             type="confirm"
             onClick={() => {
-              openModal({
-                name: 'ViewHardwarePubKey',
-                data: {
-                  desc: selectOne,
-                  Path: isTestBitCoinNetWork() ? "m/45'/1'/0'/0/0" : "m/45'/0'/0'/0/0",
-                  callback: () => {
-                    if (selectOne === 'Ledger') {
-                      return window.LedgerInterface.getPublicKey(isTestBitCoinNetWork() ? 'testnet' : 'mainnet');
-                    } else {
-                      return Promise.reject('qita');
-                    }
+              if (selectOne === 'Ledger') {
+                openModal({
+                  name: 'ViewHardwarePubKey',
+                  data: {
+                    desc: selectOne,
+                    Path: isTestBitCoinNetWork() ? "m/45'/1'/0'/0/0" : "m/45'/0'/0'/0/0",
+                    callback: () => {
+                      if (selectOne === 'Ledger') {
+                        return window.LedgerInterface.getPublicKey(isTestBitCoinNetWork() ? 'testnet' : 'mainnet');
+                      } else {
+                        return Promise.reject('qita');
+                      }
+                    },
                   },
-                },
-              });
+                });
+              }
             }}>
             <FormattedMessage id={'Confirm'} />
           </Button>
         }>
         <div className={styles.ExportHardwarePubKey}>
           <ul>
-            {[{ name: 'Ledger', icon: 'ledger' }, { name: 'Trezor', icon: 'trezor' }].map((item, index) => (
-              <li
-                className={selectOne === item.name || selectOne === item.desc ? styles.active : null}
-                key={index}
-                onClick={() => {
-                  this.setState({
-                    selectOne: item.name,
-                  });
-                }}>
-                <Icon name={item.icon} />
-                <span>{item.desc || item.name}</span>
-              </li>
-            ))}
+            {[{ name: 'Ledger', icon: 'ledger' }, { name: 'Trezor', icon: 'trezor', disabeld: true }].map(
+              (item, index) => (
+                <li
+                  className={classNames(
+                    selectOne === item.name || selectOne === item.desc ? styles.active : null,
+                    item.disabeld ? styles.disabeld : null
+                  )}
+                  key={index}
+                  onClick={() => {
+                    this.setState({
+                      selectOne: item.name,
+                    });
+                  }}>
+                  <Icon name={item.icon} />
+                  <span>{item.desc || item.name}</span>
+                </li>
+              )
+            )}
           </ul>
         </div>
       </Modal>
