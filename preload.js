d1cbb2d1b61b14eedc5080288fbe38596ba6dca8
Yongfeng LI <wliyongfeng@gmail.com>
2019-06-19 16:56:36 +0800

    Remove window.ledger

--- preload.js
+++ preload.js
@@ -1,6 +1,5 @@
 const electronStore = require("electron-store");
 const { shell, remote } = require("electron");
-const { example } = require("./MultiSign/ledger");
 const LedgerInterface = require("./MultiSign/ledger-interface");
 
 window.electronStore = function(...args) {
@@ -13,5 +12,4 @@ window.fetchFromWs = ({ url, method, params = [], timeOut = 5000 }) => {
   return remote.app.utils.fetchFromWs({ url, method, params, timeOut });
 };
 
-window.ledger = example;
 window.LedgerInterface = LedgerInterface;
