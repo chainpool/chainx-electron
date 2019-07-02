ca5e23da4496b3a5533a4495bba1a32e35a60b22
weixiaoyi <2278095567@qq.com>
2019-07-02 14:37:57 +0800

    pubKey检查fix

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -187,11 +187,15 @@ export const Patterns = {
     return /(http|https):\/\/([\w.]+\/?)\S*/.test(address) ? '' : errMsg;
   },
   isPublicKey: (pubkey, errMsg = 'NotMathTheFormat') => {
-    try {
-      bitcoin.ECPair.fromPublicKey(Buffer.from(pubkey.replace(/^0x/, ''), 'hex'), { compressed: true });
-      return '';
-    } catch (err) {
+    if (pubkey.replace(/^0x/, '').length !== 66) {
       return errMsg;
+    } else {
+      try {
+        bitcoin.ECPair.fromPublicKey(Buffer.from(pubkey.replace(/^0x/, ''), 'hex'), { compressed: true });
+        return '';
+      } catch (err) {
+        return errMsg;
+      }
     }
   },
   isHotPrivateKey: (priKey, pubKey, isTest, callback, errMsg = 'NotMathTheFormat') => {
