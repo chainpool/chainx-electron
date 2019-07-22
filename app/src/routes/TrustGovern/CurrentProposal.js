1a408a65e65b52413978461a3aa305339d1173f2
weixiaoyi <2278095567@qq.com>
2019-07-22 16:32:39 +0800

    信托治理

--- app/src/routes/TrustGovern/CurrentProposal.js
+++ app/src/routes/TrustGovern/CurrentProposal.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import * as styles from './index.less';
 import { Mixin } from '../../components';
 import ProposalSwitchTrustee from './ProposalSwitchTrustee';
-import { Mixin } from '../../components';
 import { classNames } from '../../utils';
 
 class CurrentProposal extends Mixin {
