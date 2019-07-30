96415f4445724d5ce2bfa787d4bff8c589c32ce1
weixiaoyi <2278095567@qq.com>
2019-07-30 20:58:19 +0800

    钱包下载

--- app/src/routes/components/DownLoadWallet/index.js
+++ app/src/routes/components/DownLoadWallet/index.js
@@ -4,6 +4,10 @@ import { Button, FormattedMessage, Icon, RouterGo } from '../../../components';
 import Win from '../../../resource/Win.png';
 import Mac from '../../../resource/Mac.png';
 import Linux from '../../../resource/Linux.png';
+import mathwalletIcon from '../../../resource/mathwalletIcon.png';
+import hashkeyIcon from '../../../resource/hashkeyIcon.png';
+import hashquarkIcon from '../../../resource/hashquarkIcon.png';
+
 import { Device, classNames } from '../../../utils';
 import { DowloadWalletUrl } from '../../../constants';
 
@@ -14,14 +18,18 @@ class DownLoadWallet extends Component {
         <FormattedMessage id={'DownloadWallet'} />
         <div>
           <div>
-            <div className={styles.desc}>
-              <Icon name="icon-xiazai" className={styles.downloadicon} />
-              <div className={styles.appname}>
-                <FormattedMessage id={'DesktopEndSecurityWallet'} />
-              </div>
-            </div>
-            <div className={styles.textdesc}>
-              <FormattedMessage id={'DecentralizedFullClientwallet'} />
+            {/*<div className={styles.desc}>*/}
+            {/*<Icon name="icon-xiazai" className={styles.downloadicon} />*/}
+            {/*<div className={styles.appname}>*/}
+            {/*<FormattedMessage id={'DesktopEndSecurityWallet'} />*/}
+            {/*</div>*/}
+            {/*</div>*/}
+            {/*<div className={styles.textdesc}>*/}
+            {/*<FormattedMessage id={'DecentralizedFullClientwallet'} />*/}
+            {/*</div>*/}
+
+            <div className={styles.appname}>
+              <FormattedMessage id={'DesktopEndSecurityWallet'} />
             </div>
             <ul>
               {[
@@ -46,13 +54,45 @@ class DownLoadWallet extends Component {
               ].map(item => (
                 <li key={item.name}>
                   <img src={item.src} alt={`${item.src}`} />
-                  <div>{item.name}</div>
+                  <div className={styles.name}>{item.name}</div>
 
-                  <div className={classNames(styles.button, Device.getOS() === `${item.alias}` ? styles.active : null)}>
-                    <RouterGo isOutSide go={{ pathname: item.url }} className={styles.downloadUrl}>
-                      <FormattedMessage id={'Download'} />
-                    </RouterGo>
-                  </div>
+                  {item.url && (
+                    <div
+                      className={classNames(styles.button, Device.getOS() === `${item.alias}` ? styles.active : null)}>
+                      <RouterGo isOutSide go={{ pathname: item.url }} className={styles.downloadUrl}>
+                        <FormattedMessage id={'Download'} />
+                      </RouterGo>
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+            <ul>
+              {[
+                {
+                  src: mathwalletIcon,
+                  alias: 'Math Wallet',
+                  name: 'Math Wallet',
+                  linkUrl: 'http://www.mathwallet.org/cn/',
+                },
+                {
+                  src: hashkeyIcon,
+                  alias: 'Hashkey Hub',
+                  name: 'Hashkey Hub',
+                  linkUrl: 'https://hub.hashkey.com/#/',
+                },
+                {
+                  src: hashquarkIcon,
+                  alias: 'HashQuark',
+                  name: 'HashQuark',
+                  linkUrl: 'https://www.hashquark.io/#/',
+                },
+              ].map(item => (
+                <li key={item.name}>
+                  <a href={item.linkUrl} className={styles.linkUrl} target="_blank">
+                    <img src={item.src} alt={`${item.src}`} />
+                    <div className={styles.name}>{item.name}</div>
+                  </a>
                 </li>
               ))}
             </ul>
