80cbbd3ddc67f296500e2f6a466854cf43009459
Yongfeng LI <wliyongfeng@gmail.com>
2019-07-31 16:46:17 +0800

    Fix text

--- app/src/routes/Election/Modal/VoteModal.js
+++ app/src/routes/Election/Modal/VoteModal.js
@@ -292,7 +292,7 @@ class VoteModal extends Mixin {
           )}
           {action === 'switch' && !canSwitch ? (
             <div className={styles.canSwitchHeight}>
-              下次可切换高度：{nextRenominateHeight}（预估{' '}
+              下次可切换时间：{nextRenominateHeight}（预估{' '}
               {moment_helper.formatHMS(
                 blockTime.getTime() + (nextRenominateHeight - blockNumber) * blockDuration,
                 'YYYY/MM/DD HH:mm:ss'
--- app/src/routes/Mine/Modal/ClaimConditionModal.js
+++ app/src/routes/Mine/Modal/ClaimConditionModal.js
@@ -29,7 +29,7 @@ class ClaimConditionModal extends Component {
           <div className={styles.grayBlock}>你的PCX投票冻结必须大于待领利息的10倍；且每次提息时间间隔不少于7天。</div>
           {showClaimHeight ? (
             <div className={styles.claimHeight}>
-              下次可提息高度：{claimHeight}（预估 {moment_helper.formatHMS(estimatedTime, 'YYYY/MM/DD HH:mm:ss')}）
+              下次可提息时间：{claimHeight}（预估 {moment_helper.formatHMS(estimatedTime, 'YYYY/MM/DD HH:mm:ss')}）
             </div>
           ) : null}
           {need > 0 ? (
