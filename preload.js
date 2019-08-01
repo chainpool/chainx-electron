52a512e66089c6aa4bf19076f545e13a74988525
Yongfeng LI <wliyongfeng@gmail.com>
2019-08-01 15:05:34 +0800

    Remove trezor and ledger

--- preload.js
+++ preload.js
@@ -1,7 +1,5 @@
 const electronStore = require("electron-store");
 const { shell, remote } = require("electron");
-const LedgerInterface = require("./MultiSign/ledger-interface");
-const trezorConnector = require("./MultiSign/trezor-connector");
 
 window.electronStore = function(...args) {
   return new electronStore(...args);
@@ -12,6 +10,3 @@ window.openExternal = url => shell.openExternal(url);
 window.fetchFromWs = ({ url, method, params = [], timeOut = 5000 }) => {
   return remote.app.utils.fetchFromWs({ url, method, params, timeOut });
 };
-
-window.LedgerInterface = LedgerInterface;
-window.trezorConnector = trezorConnector;
