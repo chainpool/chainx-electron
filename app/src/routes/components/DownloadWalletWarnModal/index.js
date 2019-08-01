8832cc92d024957aecb19d477a691d837720e73e
Yongfeng LI <wliyongfeng@gmail.com>
2019-08-01 10:25:11 +0800

    Correct download url

--- app/src/routes/components/DownloadWalletWarnModal/index.js
+++ app/src/routes/components/DownloadWalletWarnModal/index.js
@@ -9,15 +9,15 @@ class DownloadWalletWarnModal extends Component {
         title="下载ChainX桌面钱包"
         className={styles.DownloadWalletWarnModal}
         button={
-          <RouterGo isOutSide go={{ pathname: 'https://github.com/chainx-org/chainx-wallet/releases/tag/v1.0.0' }}>
+          <RouterGo isOutSide go={{ pathname: 'https://github.com/chainx-org/chainx-wallet/releases' }}>
             <Button size="full" type="confirm" onClick={() => {}}>
               下载
             </Button>
           </RouterGo>
         }>
         ChainX桌面钱包已经发布，您可以前往{' '}
-        <RouterGo isOutSide go={{ pathname: 'https://github.com/chainx-org/chainx-wallet/releases/tag/v1.0.0' }}>
-          https://github.com/chainx-org/chainx-wallet/releases/tag/v1.0.0{' '}
+        <RouterGo isOutSide go={{ pathname: 'https://github.com/chainx-org/chainx-wallet/releases' }}>
+          https://github.com/chainx-org/chainx-wallet/releases{' '}
         </RouterGo>
         下载使用。相比在线钱包，桌面钱包更加安全。目前的在线钱包在未来将会停止服务。
       </Modal>
