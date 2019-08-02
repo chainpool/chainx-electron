7de736c8c8c6b93dfb73a5475c772456d529214e
Yongfeng LI <wliyongfeng@gmail.com>
2019-08-02 14:02:28 +0800

    Add more translation

--- app/src/routes/Mine/Modal/ClaimConditionModal.js
+++ app/src/routes/Mine/Modal/ClaimConditionModal.js
@@ -3,6 +3,7 @@ import { Modal } from '../../../components';
 import { Warn } from '../../components';
 import * as styles from './ClaimConditionModal.less';
 import { formatNumber, moment_helper } from '../../../utils';
+import FormattedMessage from '@components/FormattedMessage';
 
 class ClaimConditionModal extends Component {
   render() {
@@ -16,33 +17,51 @@ class ClaimConditionModal extends Component {
 
     const showClaimHeight = intention.nextClaim && intention.nextClaim > blockNumber;
 
-    const ok = <p className={styles.ok}>已完成</p>;
+    const ok = (
+      <p className={styles.ok}>
+        <FormattedMessage id={'Satisfied'} />
+      </p>
+    );
     const nextClaim = (
       <p>
-        块高 {claimHeight}(预估 {moment_helper.formatHMS(intention.nextClaimTimestamp, 'YYYY/MM/DD HH:mm:ss')})
+        <FormattedMessage
+          id={'CrossChainNextClaimDetail'}
+          values={{
+            claimHeight,
+            claimTime: moment_helper.formatHMS(intention.nextClaimTimestamp, 'YYYY/MM/DD HH:mm:ss'),
+          }}
+        />
       </p>
     );
 
     const estimatedLock = <p>预估需要增加投票冻结：{formatNumber.toFixed(intention.need / Math.pow(10, 8), 8)} PCX</p>;
 
     return (
-      <Modal title={'提息条件'}>
+      <Modal title={<FormattedMessage id={'CrossChainClaimRequirements'} />}>
         <div className={styles.ClaimConditionModal}>
           <div className={styles.items}>
             <section className={styles.nextClaimHeight}>
-              <div>下次可提息时间</div>
+              <div>
+                <FormattedMessage id={'CrossChainNextClaimTime'} />
+              </div>
               {showClaimHeight ? nextClaim : ok}
             </section>
             <section>
-              <div>预估需要增加投票冻结</div>
+              <div>
+                <FormattedMessage id={'CrossChainEstimatedAddBonded'} />
+              </div>
               {typeof intention.need === 'number' && intention.need > 0 ? estimatedLock : ok}
             </section>
           </div>
 
           <Warn>
             <ol>
-              <li>1，你的PCX投票冻结必须大于等于挖矿收益（包含推荐渠道收益）的10倍.</li>
-              <li>2，每次提息时间间隔不少于7天.</li>
+              <li>
+                <FormattedMessage id={'CrossChainClaimRequirement1'} />
+              </li>
+              <li>
+                <FormattedMessage id={'CrossChainClaimRequirement2'} />
+              </li>
             </ol>
           </Warn>
         </div>
