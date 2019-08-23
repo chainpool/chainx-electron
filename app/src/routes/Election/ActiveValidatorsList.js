5beffa8ead76c0466f84dced5491a64f64f0a446
XatMassacrE <xxat.massacre@gmail.com>
2019-08-23 18:24:11 +0800

    Split ValidatorCard componnet
    and add ValidatarCard style, remove redundant store inject

--- app/src/routes/Election/ActiveValidatorsList.js
+++ app/src/routes/Election/ActiveValidatorsList.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import * as styles from './index.less';
-import { Button, FormattedMessage, LanguageContent, RouterGo } from '../../components';
-import { HoverTip, ProducerColorChange } from '../components';
-import { blockChain } from '../../constants';
-import { _, classNames, groupArrayByCount, hexPrefix, Inject } from '../../utils';
-import trustee_zh from '../../resource/trustee_zh.png';
-import trustee_en from '../../resource/trustee_en.png';
-import officialMember_zh from '../../resource/officialMember_zh.png';
-import officialMember_en from '../../resource/officialMember_en.png';
+import ValidatorCard from './ValidatorCard';
+import { ProducerColorChange } from '../components';
+import { _, Inject } from '../../utils';
 
 @Inject(({ chainStore }) => ({ chainStore }))
 class ActiveValidatorsList extends Component {
@@ -67,282 +62,36 @@ class ActiveValidatorsList extends Component {
       });
     }
 
-    const groupDataSources = groupArrayByCount(dataSourceResult, 4);
+    // const groupDataSources = groupArrayByCount(dataSourceResult, 4);
 
+    const validatorCardData = {
+      currentAddress,
+      currentAccount,
+      openModal,
+      setDefaultPrecision,
+      decodeAddressAccountId,
+      language,
+      sort,
+    };
+
+    const lastLineCount = dataSourceResult.length % 4;
     return (
-      <div className={styles.ActiveValidatorsListVersion2}>
-        <ul className={styles.list}>
-          {groupDataSources.map((one, ins) => (
-            <li
-              key={ins}
-              className={classNames(
-                styles.trs,
-                groupDataSources.length > 3 && ins > 2 && ins >= groupDataSources.length - 4 ? styles.up : null
-              )}>
-              <ul>
-                {one.map(item => (
-                  <ProducerColorChange
-                    showChange={item.isActive}
-                    Ele={'li'}
-                    key={item.name}
-                    item={item}
-                    currentChainProducer={currentChainProducer}
-                    account={encodeAddressAccountId(item.account)}
-                    {...this.props}>
-                    <div className={styles.left}>
-                      {item.imageUrl ? (
-                        <img src={item.imageUrl} width={40} height={40} alt="" />
-                      ) : (
-                        <div>{item.name[0].toUpperCase()}</div>
-                      )}
-                    </div>
-
-                    <div className={styles.right}>
-                      <div className={styles.top}>
-                        <div className={styles.nameContainer}>
-                          <div
-                            className={classNames(
-                              styles.nodeType,
-                              !item.isActive
-                                ? styles.inActive
-                                : item.isValidator
-                                ? styles.validator
-                                : styles.backupValidators
-                            )}
-                          />
-                          {/*<div*/}
-                          {/*className={classNames(*/}
-                          {/*styles.nodeType,*/}
-                          {/*!item.isActive*/}
-                          {/*? styles.inActive*/}
-                          {/*: item.isTrustee && item.isTrustee.length*/}
-                          {/*? styles.trustee*/}
-                          {/*: item.isValidator*/}
-                          {/*? styles.validator*/}
-                          {/*: styles.backupValidators*/}
-                          {/*)}*/}
-                          {/*/>*/}
-                          <div>
-                            <div className={classNames(styles.overHidden, item.myTotalVote ? styles.myVote : null)}>
-                              <span className={styles.name}> {item.name}</span>
-                              {item.isTrustee && item.isTrustee.length > 0 && (
-                                <div className={styles.trusteeImg}>
-                                  {item.isTrustee && item.isTrustee.length ? (
-                                    <FormattedMessage id={'ManageUserOutsidechainAssets'}>
-                                      {msg => (
-                                        <HoverTip tip={msg}>
-                                          <LanguageContent
-                                            zh={<img src={trustee_zh} alt="" height={14} />}
-                                            en={<img src={trustee_en} alt="" height={14} />}
-                                          />
-                                        </HoverTip>
-                                      )}
-                                    </FormattedMessage>
-                                  ) : null}
-                                </div>
-                              )}
-                              {item.isOfficialMember && (
-                                <div className={styles.isOfficialMember}>
-                                  <FormattedMessage id={'ManagePreVoteReferendum'}>
-                                    {msg => (
-                                      <HoverTip tip={msg}>
-                                        <LanguageContent
-                                          zh={<img src={officialMember_zh} alt="" height={14} />}
-                                          en={<img src={officialMember_en} alt="" height={14} />}
-                                        />
-                                      </HoverTip>
-                                    )}
-                                  </FormattedMessage>
-                                </div>
-                              )}
-
-                              {/*{item.isTrustee && item.isTrustee.length ? (*/}
-                              {/*<span className={styles.trusteeMark}>*/}
-                              {/*(<FormattedMessage id={'Trustee'} />)*/}
-                              {/*</span>*/}
-                              {/*) : null}*/}
-                            </div>
-                          </div>
-                        </div>
-                        <div className={styles.buttoncontainer}>
-                          {currentAddress ? (
-                            <Button
-                              type="confirm"
-                              className={classNames(
-                                language === 'en' ? styles.en : null,
-                                styles.votebutton,
-                                item.myTotalVote ? styles.hidden : styles.hidden
-                              )}
-                              onClick={() => {
-                                const vote = () =>
-                                  openModal({
-                                    name: 'VoteModal',
-                                    data: {
-                                      isActive: item.isActive,
-                                      target: item.account,
-                                      myTotalVote: item.myTotalVote,
-                                      isCurrentAccount: item.address === currentAccount.address,
-                                      selfVote: setDefaultPrecision(item.selfVote),
-                                      totalNomination: setDefaultPrecision(item.totalNomination),
-                                    },
-                                  });
-                                vote();
-                              }}>
-                              {item.myTotalVote ? (
-                                <FormattedMessage id={'ChangeNominate'} />
-                              ) : (
-                                <FormattedMessage id={'NominateQuick'} />
-                              )}
-                            </Button>
-                          ) : null}
-                        </div>
-                      </div>
-                      <div className={styles.down}>
-                        <div>
-                          <FormattedMessage id={'SelfIntentionBondedQuick'} />:
-                          <span>{parseInt(setDefaultPrecision(item.selfVote))}</span>
-                        </div>
-                        <div>
-                          <FormattedMessage id={'TotalVotesQuick'} />:
-                          <span>{parseInt(setDefaultPrecision(item.totalNomination))}</span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className={classNames(language === 'zh' ? styles.zh : styles.en, styles.Nodedetail)}>
-                      <ul>
-                        {item.isActive ? (
-                          <li>
-                            <div>
-                              {sort.value === 'selfVote' ? (
-                                <FormattedMessage id={'BondedRank'} />
-                              ) : (
-                                <FormattedMessage id={'VotingRank'} />
-                              )}
-                            </div>
-                            <div>
-                              <div className={styles.rank}>{item.rank}</div>
-                            </div>
-                          </li>
-                        ) : null}
-
-                        <li>
-                          <div>
-                            <FormattedMessage id={'NodeType'} />
-                          </div>
-                          <div>
-                            <div className={styles.nodetype}>
-                              {!item.isActive ? (
-                                <FormattedMessage id={'DropOut'} />
-                              ) : item.isValidator ? (
-                                <FormattedMessage id={'ValidatorNode'} />
-                              ) : (
-                                <FormattedMessage id={'StandbyNode'} />
-                              )}
-                              {/*{item.isTrustee && item.isTrustee.length ? (*/}
-                              {/*<FormattedMessage id={'TrusteeNode'} />*/}
-                              {/*) : !item.isActive ? (*/}
-                              {/*<FormattedMessage id={'DropOut'} />*/}
-                              {/*) : item.isValidator ? (*/}
-                              {/*<FormattedMessage id={'ValidatorNode'} />*/}
-                              {/*) : (*/}
-                              {/*<FormattedMessage id={'StandbyNode'} />*/}
-                              {/*)}*/}
-                            </div>
-                          </div>
-                        </li>
-                        {(item.isTrustee && item.isTrustee.length > 0) || item.isOfficialMember ? (
-                          <li>
-                            <div>
-                              <FormattedMessage id={'NodeDuty'} />
-                            </div>
-                            <div>
-                              <div className={styles.longaddress}>
-                                {item.isTrustee && item.isTrustee.length > 0 && (
-                                  <span>
-                                    <FormattedMessage id={'Trustee'} />
-                                  </span>
-                                )}
-                                {item.isTrustee && item.isTrustee.length > 0 && item.isOfficialMember && '，'}
-                                {item.isOfficialMember && (
-                                  <span>
-                                    <FormattedMessage id={'Member'} />
-                                  </span>
-                                )}
-                              </div>
-                            </div>
-                          </li>
-                        ) : null}
-
-                        <li>
-                          <div>
-                            <FormattedMessage id={'NodeWebsite'} />
-                          </div>
-                          <div>
-                            <div className={styles.longaddress}>
-                              <RouterGo isOutSide go={{ pathname: item.url }}>
-                                {item.url}
-                              </RouterGo>
-                            </div>
-                          </div>
-                        </li>
-                        <li>
-                          <div>
-                            <FormattedMessage id={'AccountAddressQuick'} />
-                          </div>
-                          <div>
-                            <div className={styles.longaddress}>
-                              <RouterGo
-                                isOutSide
-                                go={{
-                                  pathname: blockChain.chainXAccount(hexPrefix(decodeAddressAccountId(item.address))),
-                                }}>
-                                {item.address}
-                              </RouterGo>
-                            </div>
-                          </div>
-                        </li>
-                        <li>
-                          <div>
-                            <FormattedMessage id={'PoolAddress'} />
-                          </div>
-                          <div>
-                            <div className={styles.longaddress}>
-                              <RouterGo
-                                isOutSide
-                                go={{
-                                  pathname: blockChain.chainXAccount(
-                                    hexPrefix(decodeAddressAccountId(item.jackpotAddress))
-                                  ),
-                                }}>
-                                {item.jackpotAddress}
-                              </RouterGo>
-                            </div>
-                          </div>
-                        </li>
-                        <li>
-                          <div>
-                            <FormattedMessage id={'PoolAmount'} />
-                          </div>
-                          <div>
-                            <div className={styles.longaddress}>{setDefaultPrecision(item.jackpot)}</div>
-                          </div>
-                        </li>
-                        <li>
-                          <div className={styles.introduceTitle}>
-                            <FormattedMessage id={'ValidatorBrief'} />
-                          </div>
-                          <div className={styles.introduce}>
-                            <div className={styles.introducedetail}>{item.about}</div>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                  </ProducerColorChange>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
+      <div className={styles.validatorList}>
+        {dataSourceResult.map((item, index) => (
+          <div
+            className={
+              index + 1 > 4 * 4 && index + 1 > dataSourceResult.length - 3 * 4 - lastLineCount ? styles.up : null
+            }>
+            <ProducerColorChange
+              showChange={item.isActive}
+              Ele={'li'}
+              key={index}
+              currentChainProducer={currentChainProducer}
+              account={encodeAddressAccountId(item.account)}>
+              <ValidatorCard item={item} {...validatorCardData} />
+            </ProducerColorChange>
+          </div>
+        ))}
       </div>
     );
   }
