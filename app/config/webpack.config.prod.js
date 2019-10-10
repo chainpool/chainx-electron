011220694a342d021d25efa596be47cad5987144
XatMassacrE <xxat.massacre@gmail.com>
2019-10-10 17:22:51 +0800

    Update compile smaller than 20kb images to base64

--- app/config/webpack.config.prod.js
+++ app/config/webpack.config.prod.js
@@ -244,7 +244,7 @@ module.exports = {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             loader: require.resolve('url-loader'),
             options: {
-              limit: 10000,
+              limit: 20480,
               name: 'static/media/[name].[hash:8].[ext]',
             },
           },
